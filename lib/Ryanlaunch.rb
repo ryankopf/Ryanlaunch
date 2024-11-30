12d1cd0c7efd5b996a8fbab23d5d21aefea0ca07
Ryan Kopf <github@ryankopf.com>
2024-11-30 00:38:00 -0600

    Task

--- lib/Ryanlaunch.rb
+++ lib/Ryanlaunch.rb
@@ -7,3 +7,7 @@ module Ryanlaunch
   class Error < StandardError; end
   # Your code goes here...
 end
+
+if defined?(Rake)
+  Rake.application.rake_require("tasks/launch", [File.expand_path("../..", __FILE__)])
+end
