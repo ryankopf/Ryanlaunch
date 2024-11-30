55d4baeef72f3fcd54bd1684e053238b33ff88fa
Ryan Kopf <github@ryankopf.com>
2024-11-29 19:44:18 -0600

    Launch.

---
+++ lib/Ryanlaunch.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "Ryanlaunch/version"
+
+module Ryanlaunch
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/Ryanlaunch_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Ryanlaunch do
+  it "has a version number" do
+    expect(Ryanlaunch::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
